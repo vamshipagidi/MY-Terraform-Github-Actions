68446573e646249dff62eeb062a503c3975bd3d5
venu-test-at-458658971794 <venugopalreddy.pagidi@ibexlabs.com>
2023-09-25 17:51:06 +0530

    first commit

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # define region as per your account
+  region = "us-west-1" # define region as per your account
 }
 
 resource "aws_s3_bucket" "new_bucket" {
-  bucket = "demo-github-action-tf-medium"
+  bucket = "demo-github-action-tf-medium-25-09"
 
   object_lock_enabled = false
 
