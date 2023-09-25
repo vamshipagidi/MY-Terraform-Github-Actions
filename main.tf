f30f03b8fa24daf9efe367220ea8085687879f84
venu-test-at-458658971794 <venugopalreddy.pagidi@ibexlabs.com>
2023-09-25 17:35:50 +0530

    first commit

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1" # define region as per your account
+  region = "us-east-1" # define region as per your account
 }
 
 resource "aws_s3_bucket" "new_bucket" {
